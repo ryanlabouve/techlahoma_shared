eab1e4a8091c7a55a15a66ed1d2ac2159f045ba3
Jeremy Green <jeremy@octolabs.com>
2015-07-12 20:52:24 -0500

    Polish up a fake tab

--- app-shared/techlahoma.js
+++ app-shared/techlahoma.js
@@ -29,17 +29,22 @@ var TechlahomaTab = {
 
   content : ' \
     <div id="techlahoma-tab"> \
-      This is the Techlahoma tab! \
+       <img src="http://techlahoma.github.io/techlahoma_shared/techlahoma_logo.png"/>\
     </div> \
   ',
 
   styles : ' \
     #techlahoma-tab{ \
-      padding: 10px; \
-      border: 1px solid red; \
+      padding: 10px 10px 0; \
+      background: black; \
+      border: 1px solid white; \
+      border-bottom:0; \
       position:fixed; \
       bottom: 0px; \
       left: 30px; \
+      z-index:10; \
+      border-top-left-radius: 6px; \
+      border-top-right-radius: 6px; \
     } \
   '
 
