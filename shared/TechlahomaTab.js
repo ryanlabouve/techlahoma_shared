d7a1ea984e906d1b120a67daf0449ac9f9e6ff34
Jeremy Green <jeremy@octolabs.com>
2015-07-14 00:24:18 -0500

    Deploy latest stuff

---
+++ shared/TechlahomaTab.js
@@ -0,0 +1,73 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  global.TechlahomaTab = factory()
+}(this, function () { 'use strict';
+
+  // Why doesn't this next line work!?
+  //import config from '../../config/environment';
+
+
+  var TechlahomaTab = {
+
+    init : function(){
+      TechlahomaTab.addStyles();
+      TechlahomaTab.alterDom();
+      TechlahomaTab.addListeners();
+    },
+
+    alterDom : function(){
+      var element = document.getElementsByTagName("body")[0];
+
+      var div = document.createElement("div");
+      div.innerHTML = TechlahomaTab.content;
+      element.appendChild(div);
+    },
+
+    distributionPoint : function(){
+      // dirty hack
+      if(document.location.hostname=='localhost'){
+        return 'http://localhost:4200';
+      }else{
+        return 'http://shared.techlahoma.org';
+      }
+    },
+
+    addStyles : function(){
+      var link = document.createElement("link");
+      link.setAttribute('rel','stylesheet');
+      link.setAttribute('href', TechlahomaTab.distributionPoint() + '/shared/techlahoma-tab.css');
+
+      var element = document.getElementsByTagName("body")[0];
+      element.appendChild(link);
+    },
+
+    addListeners : function(){
+      document.getElementById("techlahoma-tab").addEventListener("click", function(){
+        var content = document.getElementById("techlahoma-tab-content");
+        if(content.getAttribute('class') === 'active'){
+          content.setAttribute('class','');
+        }else{
+          content.setAttribute('class','active');
+        }
+      });
+    },
+
+    content : ' \
+      <div id="techlahoma-tab-wrap"> \
+        <div id="techlahoma-tab"> \
+           <img src="http://techlahoma.github.io/techlahoma_shared/techlahoma_logo.png"/> \
+        </div> \
+        <div id="techlahoma-tab-content"> \
+           <p>Techlahoma is a non-profit organization dedicated to advancing Tech in Oklahoma. <a href="http://techlahoma.org">Learn more at Techlahoma.org</a></p> \
+        </div> \
+      </div> \
+    '
+
+  };
+
+  var techlahoma_tab = TechlahomaTab;
+
+  return techlahoma_tab;
+
+}));
